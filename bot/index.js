bdeb165ba6a2a22d52b60f6b1bfc83b4b1b30fcf
alexandrejan80 <alexandrejan80@gmail.com>
2022-05-29 22:07:05 +0200

    V3 du bot (quoi-feur, hein-2 et oui-stitti)

--- bot/index.js
+++ bot/index.js
@@ -9,9 +9,13 @@ const Client = new Discord.Client({
 Client.on('ready', () => console.log("Quoi-Feur est opérationnel !"))
 
 Client.on('messageCreate', message => {
-    if (/[QK]+U*(OI|OUA)+[\s.!;,?!]*$/i.test(message.content)) {
+    if (/[QK]+U*(OI|OUA)+[\s.!;,?!]*$/i.test(message.content))
         message.channel.send("Feur !")
-    }
+    else if (/H*(E*|A*)I+[NM]+[\s.!;,?!]*$/i.test(message.content))
+        message.channel.send("2 !")
+    else if (/O+U+I+[\s.!;,?!]*$/i.test(message.content))
+        message.channel.send("Stitti !")
 })
 
 Client.login("OTc5ODEzNjYzNTUwNjE5Njc4.GbhndW.2zmLIVJPYwAkgkNYrCWL5tc1qvME7ikwX5s_7k")
+
